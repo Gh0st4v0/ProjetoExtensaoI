24bd86d7e7e398653af9bc72b0a06bed224ab49b
Gh0st4v0 <gustavo.135.andrade@gmail.com>
2026-06-03 13:25:40 -0300

    adding user migration

---
+++ Source/Server/SpringBootApp/src/main/java/com/example/SpringBootApp/services/UsuarioService.java
@@ -0,0 +1,10 @@
+package com.example.SpringBootApp.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+}
